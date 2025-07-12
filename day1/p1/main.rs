93d51ea8cb45d27e0dd792d2789a1a451c4e6b05
fuzz88 <ivan@oschepkov.ru>
2025-07-12 19:46:13 +0800

    [day1] wip

--- day1/p1/main.rs
+++ day1/p1/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("hello, world");
+use std::env;
+use std::error;
+use std::fs;
+
+fn read_input(filename: &str) -> Result<String, Box<dyn error::Error>> {
+    let content = fs::read_to_string(filename)?;
+    Ok(content)
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("--- Day 1: Inverse Captcha ---");
+
+    let input_file = env::args()
+        .nth(1)
+        .ok_or("no input file name as cli argument is provided")?;
+
+    let input_data = read_input(&input_file)?;
+
+    println!("{}", input_data);
+
+    Ok(())
 }
