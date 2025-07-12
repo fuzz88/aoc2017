9a294d111d98fe0104175604b92b8f32aa623df7
fuzz88 <ivan@oschepkov.ru>
2025-07-12 23:55:18 +0800

    [day3] p1 solved

--- day3/p1/main.rs
+++ day3/p1/main.rs
@@ -1,9 +1,39 @@
 use std::error;
 
+fn part1(addr: u32) -> u32 {
+    if addr == 1 {
+        return 0;
+    }
+
+    let mut q = 1;
+    let k;
+
+    let side = loop {
+        // previous powers could be memorized, but still ok for the task.
+        // compiler must get powers out of the loop as invariants though.
+        if u32::pow(q, 2) >= addr {
+            // println!("square = {}", q);
+            k = (addr - u32::pow(q - 2, 2) + 1) % (u32::pow(q, 2) - u32::pow(q - 2, 2));
+            // println!("idx on square = {}", k);
+            break k / q;
+        }
+        q += 2
+    };
+
+    // println!("side = {}", side);
+    let shift = (k - (q-1) * side - (q - 1) / 2 - 1) as i32;
+    // println!("shift = {}", shift);
+    let walls = (q - 1) / 2;
+
+    i32::abs(shift) as u32 + walls 
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day 3: Spiral Memory ---");
 
     let input_data = 325489;
+
+    println!("{}", part1(input_data));
 
     Ok(())
 }
