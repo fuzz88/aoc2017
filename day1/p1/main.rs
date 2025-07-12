fb9b62bfea1ab21f51e9348d0901a2ee75b11f4e
fuzz88 <ivan@oschepkov.ru>
2025-07-12 20:49:09 +0800

    [day1] solved

--- day1/p1/main.rs
+++ day1/p1/main.rs
@@ -1,22 +1,46 @@
-use std::env;
-use std::error;
-use std::fs;
+#![feature(iter_map_windows)]
 
-fn read_input(filename: &str) -> Result<String, Box<dyn error::Error>> {
-    let content = fs::read_to_string(filename)?;
-    Ok(content)
+use std::cmp::Ordering;
+
+fn input() -> &'static [u8] {
+    include_bytes!("input.txt")
 }
 
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn part1(seq: &[u8]) -> u32 {
+    seq[..seq.len() - 1]
+        .iter()
+        .map_windows(|[a, b]| match a.cmp(b) {
+            Ordering::Equal => (*a - 48) as u32,
+            _ => 0,
+        })
+        .sum::<u32>()
+        + if seq[0] == seq[seq.len() - 2] {
+            (seq[0] - 48) as u32
+        } else {
+            0
+        }
+}
+
+fn part2(seq: &[u8]) -> u32 {
+    let mut result = 0;
+    let seq = &seq[..seq.len() - 1];
+    // println!("{}", seq.len());
+    let shift = seq.len() / 2;
+
+    for (idx, byte) in seq.iter().enumerate() {
+        let digit = byte - 48; // ascii
+        let next_digit = seq[(idx + shift) % seq.len()] - 48;
+        if digit == next_digit {
+            result += digit as u32;
+        }
+    }
+
+    result
+}
+
+fn main() {
     println!("--- Day 1: Inverse Captcha ---");
 
-    let input_file = env::args()
-        .nth(1)
-        .ok_or("no input file name as cli argument is provided")?;
-
-    let input_data = read_input(&input_file)?;
-
-    println!("{}", input_data);
-
-    Ok(())
+    println!("{:?}", part1(input()));
+    println!("{:?}", part2(input()));
 }
