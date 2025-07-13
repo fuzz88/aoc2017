f9fc513bebc93e5b970acaa7be136ac4bba2125d
fuzz88 <ivan@oschepkov.ru>
2025-07-13 17:18:53 +0800

    [day5] solved

--- day5/p1/main.rs
+++ day5/p1/main.rs
@@ -1,8 +1,6 @@
-use std::fs;
-use std::env;
-use std::error;
+use std::{env, error, fs, io};
 
-fn read_input(filename: &str) -> Result<Vec<i32>, Box<dyn error::Error>> {
+fn read_input(filename: &str) -> Result<Vec<i32>, io::Error> {
     let instructions = fs::read_to_string(filename)?
         .lines()
         .map(|line| line.parse().unwrap())
@@ -11,14 +9,56 @@ fn read_input(filename: &str) -> Result<Vec<i32>, Box<dyn error::Error>> {
     Ok(instructions)
 }
 
-fn main() -> Result<(), Box<dyn error::Error>>{
+fn evaluate<F>(instructions: &mut Vec<i32>, mut modify: F) -> u32
+where
+    F: FnMut(&mut Vec<i32>, i32, i32),
+{
+    let mut steps = 0;
+    let mut pc: i32 = 0;
+
+    loop {
+        let jump = instructions[pc as usize];
+        modify(instructions, pc, jump);
+        pc += jump;
+        assert!(pc >= 0, "invalid instructions");
+
+        steps += 1;
+
+        if pc as usize >= instructions.len() {
+            break steps;
+        }
+    }
+}
+
+fn part1(mut instructions: Vec<i32>) -> u32 {
+    evaluate(&mut instructions, |instructions, pc, _jump| {
+        instructions[pc as usize] += 1
+    })
+}
+
+fn part2(mut instructions: Vec<i32>) -> u32 {
+    evaluate(&mut instructions, |instructions, pc, jump| {
+        if jump >= 3 {
+            instructions[pc as usize] -= 1;
+        } else {
+            instructions[pc as usize] += 1;
+        }
+    })
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day 5: A Maze of Twisty Trampolines, All Alike ---");
 
-    let input_file = env::args().nth(1).ok_or("no input file as cli argument is provided")?;
+    let input_file = env::args()
+        .nth(1)
+        .ok_or("no input file as cli argument is provided")?;
 
     let input_data = read_input(&input_file)?;
 
-    println!("{:?}", input_data);
+    // println!("{:?}", input_data);
+
+    println!("{}", part1(input_data.clone()));
+    println!("{}", part2(input_data.clone()));
 
     Ok(())
 }
