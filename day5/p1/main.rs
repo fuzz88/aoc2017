e4f35ceb87b02da8f35dae95da27f444ff8c304c
fuzz88 <ivan@oschepkov.ru>
2025-07-13 14:17:58 +0800

    [day5] reading input

--- day5/p1/main.rs
+++ day5/p1/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("hello, world");
+use std::fs;
+use std::env;
+use std::error;
+
+fn read_input(filename: &str) -> Result<Vec<i32>, Box<dyn error::Error>> {
+    let instructions = fs::read_to_string(filename)?
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    Ok(instructions)
+}
+
+fn main() -> Result<(), Box<dyn error::Error>>{
+    println!("--- Day 5: A Maze of Twisty Trampolines, All Alike ---");
+
+    let input_file = env::args().nth(1).ok_or("no input file as cli argument is provided")?;
+
+    let input_data = read_input(&input_file)?;
+
+    println!("{:?}", input_data);
+
+    Ok(())
 }
