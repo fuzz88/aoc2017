f420b6ff99f05b93fde1fea2aaf0e2f4f7f78871
fuzz88 <ivan@oschepkov.ru>
2025-07-13 10:45:36 +0800

    [day4] reading input

--- day4/p1/main.rs
+++ day4/p1/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("hello, world");
+use std::collections::HashMap;
+use std::env;
+use std::error;
+use std::fs;
+
+type Passphrase = Vec<String>;
+
+fn read_input(filename: &str) -> Result<Vec<Passphrase>, Box<dyn error::Error>> {
+    let passphrases = fs::read_to_string(filename)?
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|word| word.to_string())
+                .collect()
+        })
+        .collect();
+
+    Ok(passphrases)
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("--- Day 4: High-Entropy Passphrases ---");
+
+    let input_file = env::args()
+        .nth(1)
+        .ok_or("no input file as cli argument is provided")?;
+
+    let input_data = read_input(&input_file)?;
+
+    println!("{input_data:?}");
+
+    Ok(())
 }
