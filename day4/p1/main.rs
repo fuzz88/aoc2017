8fbd7dc588237b845ba090f46df217f589142006
fuzz88 <ivan@oschepkov.ru>
2025-07-13 12:41:09 +0800

    [day4] solved
    [day4] cleanup

--- day4/p1/main.rs
+++ day4/p1/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::env;
 use std::error;
 use std::fs;
+use std::hash::Hash;
 
 type Passphrase = Vec<String>;
 
@@ -18,6 +19,61 @@ fn read_input(filename: &str) -> Result<Vec<Passphrase>, Box<dyn error::Error>> 
     Ok(passphrases)
 }
 
+fn freqs<T: Eq + Hash + Clone>(items: &Vec<T>) -> HashMap<T, u32> {
+    let mut freqs = HashMap::<T, u32>::new();
+
+    for item in items {
+        let freq = freqs.entry(item.clone()).or_insert(0);
+        *freq += 1;
+    }
+
+    freqs
+}
+
+fn is_valid_1(pp: &&Passphrase) -> bool {
+    freqs::<String>(pp).iter().all(|(_, freq)| *freq == 1)
+}
+
+fn part1(pps: &Vec<Passphrase>) -> usize {
+    pps.iter().filter(is_valid_1).count()
+}
+
+fn equal_freqs<T: Eq + Hash>(freq1: &HashMap<T, u32>, freq2: &HashMap<T, u32>) -> bool {
+    freq1.iter().all(|(ch, freq)| {
+        let value = freq2.get(ch);
+        value == Some(freq)
+    }) && freq2.iter().all(|(ch, freq)| {
+        let value = freq1.get(ch);
+        value == Some(freq)
+    })
+}
+
+fn is_valid_2(pp: &&Passphrase) -> bool {
+    let word_freqs = freqs::<String>(pp);
+    let uniq_words = word_freqs.iter().all(|(_, freq)| *freq == 1);
+    if !uniq_words {
+        return false;
+    }
+    let char_freqs: Vec<_> = pp
+        .iter()
+        .map(|word| freqs::<char>(&word.chars().collect()))
+        .collect();
+
+    // annagrams have the same char frequencies
+    for (idx1, freq1) in char_freqs.iter().enumerate() {
+        for (idx2, freq2) in char_freqs.iter().enumerate() {
+            if idx1 != idx2 && equal_freqs::<char>(freq1, freq2) {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn part2(pps: &Vec<Passphrase>) -> usize {
+    pps.iter().filter(is_valid_2).count()
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day 4: High-Entropy Passphrases ---");
 
@@ -27,7 +83,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let input_data = read_input(&input_file)?;
 
-    println!("{input_data:?}");
+    // println!("{input_data:?}");
+
+    println!("{}", part1(&input_data));
+    println!("{}", part2(&input_data));
 
     Ok(())
 }
