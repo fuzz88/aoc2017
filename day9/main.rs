1b9b8c728b61249d7aeecbd4a7cd26229b4ba8e3
fuzz88 <ivan@oschepkov.ru>
2025-07-14 23:30:07 +0800

    [day9] reinit

--- day9/main.rs
+++ day9/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("hello, world");
+use std::env;
+use std::error;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("--- Day9: Stream Processing ---");
+
+    let input_file = env::args()
+        .nth(1)
+        .ok_or("no input file as cli argument is provided")?;
+    println!("{}", input_file);
+
+    Ok(())
 }
