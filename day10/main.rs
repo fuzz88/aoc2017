17e1bf45173ffceb3aa51a849e8bcb84446473e5
fuzz88 <ivan@oschepkov.ru>
2025-07-16 21:29:16 +0800

    [day10] reading input

--- day10/main.rs
+++ day10/main.rs
@@ -1,5 +1,32 @@
 use std::env;
 use std::error;
+use std::fs;
+
+fn read_input(filename: &str) -> Result<Vec<u32>, Box<dyn error::Error>> {
+    let lengths = fs::read_to_string(filename)?
+        .split(',')
+        .map(|l| l.trim_end().parse().unwrap())
+        // .inspect(|l| println!("{}", l))
+        .collect();
+
+    Ok(lengths)
+}
+
+fn reverse(list: &Vec<u32>, position: usize, length: usize) -> &Vec<u32> {
+    todo!()
+}
+
+fn part1(input: &Vec<u32>) -> u32 {
+    let mut input = input.clone();
+    let size = input.pop().unwrap() as usize;
+
+    let mut list = vec![];
+    (0..size).for_each(|num| list.push(num));
+
+    println!("{:?}", list);
+
+    0
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day10: Knot Hash ---");
@@ -7,7 +34,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let input_file = env::args()
         .nth(1)
         .ok_or("no input file as cli argument is provided")?;
-    println!("{}", input_file);
+
+    let input_data = read_input(&input_file)?;
+
+    println!("{}", part1(&input_data));
 
     Ok(())
 }
