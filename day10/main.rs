f0412bb454d2fc1513e781e454d51d42d97240a9
fuzz88 <ivan@oschepkov.ru>
2025-07-17 00:51:41 +0800

    [day10] cleanup

--- day10/main.rs
+++ day10/main.rs
@@ -6,36 +6,31 @@ fn read_input(filename: &str) -> Result<Vec<usize>, Box<dyn error::Error>> {
     let lengths = fs::read_to_string(filename)?
         .split(',')
         .map(|l| l.trim_end().parse().unwrap())
-        // .inspect(|l| println!("{}", l))
         .collect();
 
     Ok(lengths)
 }
 
 fn reverse(list: &mut Vec<u8>, position: usize, length: usize) {
-    // println!("{} {}", position, length);
-    // println!("{:?}", list);
-
     let end_pos = position + length - 1;
 
-    let mut i = position % list.len();
-    let mut j = end_pos % list.len();
-    while i != (position + length / 2) % list.len() {
-        // println!("{} {}", i, j);
-        if i != j {
-            let t = list[i];
-            list[i] = list[j];
-            list[j] = t;
-        }
-        i += 1;
-        i = i % list.len();
-        if j == 0 {
-            j = list.len();
-        }
-        j -= 1;
-    }
+    let mut l = position % list.len();
+    let mut h = end_pos % list.len();
 
-    // println!("{:?}", list);
+    while l != (position + length / 2) % list.len() {
+        if l != h {
+            // swap
+            list[l] = list[h] ^ list[l];
+            list[h] = list[l] ^ list[h];
+            list[l] = list[h] ^ list[l]
+        }
+        l += 1;
+        l = l % list.len();
+        if h == 0 {
+            h = list.len();
+        }
+        h -= 1;
+    }
 }
 
 fn part1(input: &Vec<usize>) -> u16 {
@@ -45,6 +40,7 @@ fn part1(input: &Vec<usize>) -> u16 {
     let mut skip = 0;
     let mut position = 0;
 
+    // one round
     for length in input {
         reverse(&mut list, position, *length);
         position += (*length + skip) % list.len();
@@ -54,45 +50,39 @@ fn part1(input: &Vec<usize>) -> u16 {
     list[0] as u16 * list[1] as u16
 }
 
-fn part2() -> String {
-    let lengths = include_bytes!("input.txt");
-    let mut lengths: Vec<usize> = lengths[0..lengths.len() - 1]
+fn part2(input: &[u8]) -> String {
+    let mut lengths: Vec<usize> = input[0..input.len() - 1]
         .iter()
         .copied()
         .map(|v| v as usize)
         .collect();
 
     lengths.extend([17, 31, 73, 47, 23]);
-    // println!("{:?}", lengths);
 
-    let mut sparse = vec![];
-    (0..=255).for_each(|num| sparse.push(num as u8));
+    let mut list = vec![];
+    (0..=255).for_each(|num| list.push(num as u8));
 
     let mut position = 0;
     let mut skip = 0;
 
+    // 64 rounds
     (0..64).for_each(|_| {
         for length in &lengths {
-            reverse(&mut sparse, position, *length);
-            position += (*length + skip) % sparse.len();
+            reverse(&mut list, position, *length);
+            position += (*length + skip) % list.len();
             skip += 1;
         }
     });
 
-    let dense: Vec<u8> = sparse
+    let hash = list
         .chunks(16)
+        // densify [256] -> [16]
         .map(|chunk| chunk.iter().copied().reduce(|acc, e| acc ^ e).unwrap())
-        .collect();
-
-    // println!("{:?}", list);
-
-    let hash = dense
-        .iter()
+        // represent as 32-char hexidecimal string
         .map(|num| format!("{num:02x}"))
-        .collect::<Vec<_>>()
+        .collect::<Vec<String>>()
         .join("");
 
-    // println!("{hash}");
     hash
 }
 
@@ -106,7 +96,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let input_data = read_input(&input_file)?;
 
     println!("{}", part1(&input_data));
-    println!("{}", part2());
+    println!("{}", part2(include_bytes!("input.txt")));
 
     Ok(())
 }
