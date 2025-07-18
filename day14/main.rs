c33d4d1c880daaf717064ae59a864321413dbf8d
fuzz88 <ivan@oschepkov.ru>
2025-07-18 23:07:20 +0800

    [day14] solved

--- day14/main.rs
+++ day14/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::env;
 use std::error;
 use std::fs;
@@ -29,6 +30,7 @@ fn reverse(list: &mut Vec<u8>, position: usize, length: usize) {
 }
 
 fn part1(input: &String) -> u32 {
+    // Given your actual key string, how many squares are used?
     (0..128)
         .map(|idx| {
             let mut to_hash = input.clone();
@@ -59,7 +61,8 @@ fn part1(input: &String) -> u32 {
                 }
             });
 
-            let used: u32 = list.chunks(16)
+            let used: u32 = list
+                .chunks(16)
                 // densify [256] -> [16]
                 .map(|chunk| chunk.iter().copied().reduce(|acc, e| acc ^ e).unwrap())
                 .map(|num| num.count_ones())
@@ -68,6 +71,102 @@ fn part1(input: &String) -> u32 {
             used
         })
         .sum()
+}
+
+fn region_mark(disk: &mut [bool; 128 * 128], col: usize, row: usize) {
+    let mut to_process = VecDeque::new();
+
+    // println!("{} {}", row, col);
+    to_process.push_back((col, row));
+
+    while let Some(coords) = to_process.pop_front() {
+        let (col, row) = coords;
+        disk[row * 128 + col] = false;
+        // println!("marked");
+        for (dr, dc) in [(0, 1), (1, 0), (0, !0), (!0, 0)] {
+            // https://t.me/bminaiev_blog/59
+            let nrow = row.overflowing_add(dr).0;
+            let ncol = col.overflowing_add(dc).0;
+            // println!("{} {}", ncol, nrow);
+            // todo!();
+            if nrow < 128 && ncol < 128 {
+                if disk[nrow * 128 + ncol] {
+                    to_process.push_back((ncol, nrow));
+                }
+            }
+        }
+    }
+}
+
+fn part2(input: &String) -> u32 {
+    // How many regions are present given your key string?
+    let mut disk = [false; 128 * 128];
+
+    (0..128).for_each(|idx| {
+        let mut to_hash = input.clone();
+        to_hash.push('-');
+        to_hash.push_str(&idx.to_string());
+
+        let mut lengths: Vec<usize> = to_hash
+            .as_bytes()
+            .iter()
+            .copied()
+            .map(|v| v as usize)
+            .collect();
+
+        lengths.extend([17, 31, 73, 47, 23]);
+
+        let mut list = vec![];
+        (0..=255).for_each(|num| list.push(num as u8));
+
+        let mut position = 0;
+        let mut skip = 0;
+
+        // 64 rounds
+        (0..64).for_each(|_| {
+            for length in &lengths {
+                reverse(&mut list, position, *length);
+                position += (*length + skip) % list.len();
+                skip += 1;
+            }
+        });
+
+        let mut column: Vec<u8> = list
+            .chunks(16)
+            // densify [256] -> [16]
+            .map(|chunk| chunk.iter().copied().reduce(|acc, e| acc ^ e).unwrap())
+            .collect();
+
+        let mut row = 128;
+        let mut col = 0;
+
+        while row != 0 {
+            if row % 8 == 0 {
+                col = column.pop().unwrap();
+            }
+            disk[idx * 128 + row - 1] = (col != 0) && col % 2 != 0;
+            if col != 0 {
+                col = col / 2;
+            }
+            row -= 1;
+        }
+    });
+
+    // println!("{:?}", disk);
+    let mut regions_count = 0;
+
+    for row in 0..128 {
+        for col in 0..128 {
+            if disk[row * 128 + col] {
+                regions_count += 1;
+                region_mark(&mut disk, col, row);
+            }
+        }
+    }
+
+    // println!("{:?}", disk);
+
+    regions_count
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -80,6 +179,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let input_data = read_input(&input_file)?;
 
     println!("{}", part1(&input_data));
+    println!("{}", part2(&input_data));
 
     Ok(())
 }
