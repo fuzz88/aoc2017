78c1adadee551c7e5705834109e59d27f6d61432
fuzz88 <ivan@oschepkov.ru>
2025-07-19 12:11:11 +0800

    [day14] BitField instead of booleans

--- day14/main.rs
+++ day14/main.rs
@@ -3,6 +3,41 @@ use std::env;
 use std::error;
 use std::fs;
 
+#[derive(Debug)]
+struct BitField128 {
+    arr: [u8; 2048],
+}
+
+impl BitField128 {
+    fn from_vec(numbers: &[u8]) -> Self {
+        assert!(numbers.len() == 2048);
+
+        let mut arr: [u8; 2048] = [0; 2048];
+
+        for idx in 0..2048 {
+            arr[idx] = numbers[idx];
+        }
+
+        BitField128 { arr }
+    }
+
+    fn is_used(&self, col: usize, row: usize) -> bool {
+        let idx = row * 128 + col;
+        let n_idx = idx / 8;
+        let shift = 7 - idx % 8;
+
+        self.arr[n_idx] & (1 << shift) != 0
+    }
+
+    fn set_unused(&mut self, col: usize, row: usize) {
+        let idx = row * 128 + col;
+        let n_idx = idx / 8;
+        let shift = 7 - idx % 8;
+
+        self.arr[n_idx] = self.arr[n_idx] & !(1 << shift);
+    }
+}
+
 fn read_input(filename: &str) -> Result<String, Box<dyn error::Error>> {
     Ok(fs::read_to_string(filename)?.trim_end().to_string())
 }
@@ -16,9 +51,9 @@ fn reverse(list: &mut Vec<u8>, position: usize, length: usize) {
     while l != (position + length / 2) % list.len() {
         if l != h {
             // swap
-            list[l] = list[h] ^ list[l];
+            list[l] = list[l] ^ list[h];
             list[h] = list[l] ^ list[h];
-            list[l] = list[h] ^ list[l]
+            list[l] = list[l] ^ list[h]
         }
         l += 1;
         l = l % list.len();
@@ -46,8 +81,8 @@ fn part1(input: &String) -> u32 {
 
             lengths.extend([17, 31, 73, 47, 23]);
 
-            let mut list = vec![];
-            (0..=255).for_each(|num| list.push(num as u8));
+            let mut hash = vec![];
+            (0..=255).for_each(|num| hash.push(num as u8));
 
             let mut position = 0;
             let mut skip = 0;
@@ -55,13 +90,13 @@ fn part1(input: &String) -> u32 {
             // 64 rounds
             (0..64).for_each(|_| {
                 for length in &lengths {
-                    reverse(&mut list, position, *length);
-                    position += (*length + skip) % list.len();
+                    reverse(&mut hash, position, *length);
+                    position += (*length + skip) % hash.len();
                     skip += 1;
                 }
             });
 
-            let used: u32 = list
+            let used: u32 = hash
                 .chunks(16)
                 // densify [256] -> [16]
                 .map(|chunk| chunk.iter().copied().reduce(|acc, e| acc ^ e).unwrap())
@@ -73,20 +108,20 @@ fn part1(input: &String) -> u32 {
         .sum()
 }
 
-fn region_mark(disk: &mut [bool; 128 * 128], col: usize, row: usize) {
+fn region_mark(disk: &mut BitField128, col: usize, row: usize) {
     let mut to_process = VecDeque::new();
 
     to_process.push_back((col, row));
 
     while let Some((col, row)) = to_process.pop_front() {
-        disk[row * 128 + col] = false;
+        disk.set_unused(col, row);
 
         for (dr, dc) in [(0, 1), (1, 0), (0, !0), (!0, 0)] {
             // https://t.me/bminaiev_blog/59
             let nrow = row.overflowing_add(dr).0;
             let ncol = col.overflowing_add(dc).0;
 
-            if nrow < 128 && ncol < 128 && disk[nrow * 128 + ncol] {
+            if nrow < 128 && ncol < 128 && disk.is_used(ncol, nrow) {
                 to_process.push_back((ncol, nrow));
             }
         }
@@ -95,7 +130,8 @@ fn region_mark(disk: &mut [bool; 128 * 128], col: usize, row: usize) {
 
 fn part2(input: &String) -> u32 {
     // How many regions are present given your key string?
-    let mut disk = [false; 128 * 128];
+    //
+    let mut disk = Vec::<u8>::new();
 
     (0..128).for_each(|idx| {
         let mut to_hash = input.clone();
@@ -111,8 +147,8 @@ fn part2(input: &String) -> u32 {
 
         lengths.extend([17, 31, 73, 47, 23]);
 
-        let mut list = vec![];
-        (0..=255).for_each(|num| list.push(num as u8));
+        let mut hash = vec![];
+        (0..=255).for_each(|num| hash.push(num as u8));
 
         let mut position = 0;
         let mut skip = 0;
@@ -120,38 +156,25 @@ fn part2(input: &String) -> u32 {
         // 64 rounds
         (0..64).for_each(|_| {
             for length in &lengths {
-                reverse(&mut list, position, *length);
-                position += (*length + skip) % list.len();
+                reverse(&mut hash, position, *length);
+                position += (*length + skip) % hash.len();
                 skip += 1;
             }
         });
 
-        let mut row: Vec<u8> = list
-            .chunks(16)
+        hash.chunks(16)
             // densify [256] -> [16]
             .map(|chunk| chunk.iter().copied().reduce(|acc, e| acc ^ e).unwrap())
-            .collect();
-
-        let mut col_n = 128;
-        let mut bits = 0; // unused 0, but must be initialized at this point.
-
-        while col_n != 0 {
-            if col_n % 8 == 0 {
-                bits = row.pop().unwrap();
-            }
-            disk[idx * 128 + col_n - 1] = (bits != 0) && bits & 1 == 1;
-            if bits != 0 {
-                bits = bits >> 1;
-            }
-            col_n -= 1;
-        }
+            .for_each(|num| disk.push(num));
     });
+
+    let mut disk = BitField128::from_vec(&disk);
 
     let mut regions_count = 0;
 
     for row in 0..128 {
         for col in 0..128 {
-            if disk[row * 128 + col] {
+            if disk.is_used(col, row) {
                 regions_count += 1;
                 region_mark(&mut disk, col, row);
             }
