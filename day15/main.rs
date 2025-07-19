cb25e34143fc0cb0506a2a7b7b06057c0c0884b7
fuzz88 <ivan@oschepkov.ru>
2025-07-19 20:19:24 +0800

    [day15] p1 solved

--- day15/main.rs
+++ day15/main.rs
@@ -4,27 +4,52 @@ use std::fs;
 
 #[derive(Debug)]
 struct Generator {
-    current_value: u32,
-    factor: u32,
-    divider: u32,
+    current_value: u64,
+    factor: u64,
+    divider: u64,
 }
 
 impl Iterator for Generator {
-    type Item = u32;
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.current_value = self.current_value * self.factor % self.divider;
+        self.current_value = (self.current_value) * self.factor % self.divider;
         Some(self.current_value)
     }
 }
 
-fn read_input(filename: &str) -> Result<Vec<u32>, Box<dyn error::Error>> {
+fn read_input(filename: &str) -> Result<Vec<u64>, Box<dyn error::Error>> {
     let init_values = fs::read_to_string(filename)?
         .lines()
         .map(|line| line.split_whitespace().last().unwrap().parse().unwrap())
         .collect();
 
     Ok(init_values)
+}
+
+fn part1(init_values: &[u64]) -> usize {
+    let generator_a = Generator {
+        current_value: init_values[0],
+        factor: 16807,
+        divider: 2147483647,
+    };
+    let generator_b = Generator {
+        current_value: init_values[1],
+        factor: 48271,
+        divider: 2147483647,
+    };
+
+    generator_a
+        .zip(generator_b)
+        .take(40_000_000)
+        .map(|(a, b)| {
+            if ((a << 48) >> 48) == ((b << 48) >> 48) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -36,22 +61,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let input_data = read_input(&input_file)?;
 
-    let mut generator_a = Generator {
-        current_value: input_data[0],
-        factor: 16807,
-        divider: 2147483647,
-    };
-    let mut generator_b = Generator {
-        current_value: input_data[1],
-        factor: 48271,
-        divider: 2147483647,
-    };
-
-    println!(
-        "{} {}",
-        generator_a.next().unwrap(),
-        generator_b.next().unwrap()
-    );
+    println!("{}", part1(&input_data));
 
     Ok(())
 }
