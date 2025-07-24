ce2b059a5f2f6d212d231af88f9860978375c4ec
fuzz88 <ivan@oschepkov.ru>
2025-07-24 14:00:52 +0800

    [day21] reading input

--- day21/main.rs
+++ day21/main.rs
@@ -1,5 +1,18 @@
 use std::env;
 use std::error;
+use std::fs;
+
+type Rule = (Vec<u8>, Vec<u8>);
+
+fn read_input(filename: &str) -> Result<Vec<Rule>, Box<dyn error::Error>> {
+    let rules = fs::read_to_string(filename)?
+        .lines()
+        .map(|line| line.split(" => ").map(|s| s.as_bytes().to_owned()))
+        .map(|mut bytes| (bytes.next().unwrap(), bytes.next().unwrap()))
+        .collect();
+
+    Ok(rules)
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day21: Fractal Art ---");
@@ -8,7 +21,9 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         .nth(1)
         .ok_or("no input file as cli argument is provided")?;
 
-    println!("{}", input_file);
+    let input_data = read_input(&input_file)?;
+
+    println!("{:?}", input_data);
 
     Ok(())
 }
