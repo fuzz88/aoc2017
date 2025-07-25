19bd7ca29f0144e20a7a503f0b77cedfef3548d3
fuzz88 <ivan@oschepkov.ru>
2025-07-25 20:57:59 +0800

    [day21] proper transformations

--- day21/main.rs
+++ day21/main.rs
@@ -27,61 +27,77 @@ fn print_image(image: &[u8]) {
     print!("\n");
 }
 
-fn flip_vertically(image: &[u8]) -> Vec<u8> {
-    image
-        .split(|p| *p == 47)
-        .rev()
-        .collect::<Vec<_>>()
-        .join(&47)
-        .to_owned()
-}
-
-fn flip_horizontally(image: &[u8]) -> Vec<u8> {
-    image
-        .split(|p| *p == 47)
-        .map(|row| row.iter().copied().rev().collect::<Vec<_>>())
-        .collect::<Vec<_>>()
-        .join(&47)
-        .to_owned()
-}
-
-fn transpose(image: &[u8]) -> Vec<u8> {
-    let n = usize::isqrt(image.len());
-
-    let mut result = image
-        .split(|p| *p == 47)
-        .map(|row| row.iter().copied().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    for i in 0..n {
-        for j in i..n {
-            let t = result[i][j];
-            result[i][j] = result[j][i];
-            result[j][i] = t;
+fn flip_vertically(image: &mut [u8], n: usize) {
+    for i in 0..n / 2 {
+        for j in 0..n {
+            let idx_a = i * (n + 1) + j;
+            let idx_b = (n + 1) * (n - i - 1) + j;
+            mem::swap(
+                unsafe { image.as_mut_ptr().add(idx_a).as_mut().unwrap() },
+                unsafe { image.as_mut_ptr().add(idx_b).as_mut().unwrap() },
+            );
         }
     }
-
-    result.join(&47).iter().copied().collect::<Vec<_>>()
 }
 
-fn rotate_ccw(image: &[u8]) -> Vec<u8> {
-    flip_vertically(&transpose(image))
+fn flip_horizontally(image: &mut [u8], n: usize) {
+    for i in 0..n {
+        for j in 0..n / 2 {
+            let idx_a = i * (n + 1) + j;
+            let idx_b = i * (n + 1) + (n - j - 1);
+            mem::swap(
+                unsafe { image.as_mut_ptr().add(idx_a).as_mut().unwrap() },
+                unsafe { image.as_mut_ptr().add(idx_b).as_mut().unwrap() },
+            );
+        }
+    }
+}
+
+fn transpose(image: &mut [u8], n: usize) {
+    for i in 0..n {
+        for j in i..n {
+            let idx_a = i * (n + 1) + j;
+            let idx_b = j * (n + 1) + i;
+            mem::swap(
+                unsafe { image.as_mut_ptr().add(idx_a).as_mut().unwrap() },
+                unsafe { image.as_mut_ptr().add(idx_b).as_mut().unwrap() },
+            );
+        }
+    }
+}
+
+fn rotate_ccw(image: &mut [u8], n: usize, count: usize) {
+    for _ in 0..count {
+        transpose(image, n);
+        flip_vertically(image, n);
+    }
 }
 
 fn match_rule(image: &[u8], rule: &Rule) -> usize {
-    print_image(image);
-    println!();
-    print_image(&rule.0);
-    println!();
-    print_image(&flip_vertically(&rule.0));
-    println!();
-    print_image(&flip_horizontally(&rule.0));
-    println!();
-    print_image(&rotate_ccw(&rule.0));
-    println!();
-    print_image(&rotate_ccw(&rotate_ccw(&rule.0)));
-    println!();
-    print_image(&rotate_ccw(&rotate_ccw(&rotate_ccw(&rule.0))));
+    let n = usize::isqrt(image.len());
+
+    print_image(&image);
+    println!("flipping horizontally:");
+    let mut image1 = image.to_owned();
+    flip_horizontally(&mut image1, n);
+    print_image(&image1);
+    println!("flipping vertically:");
+    let mut image2 = image.to_owned();
+    flip_vertically(&mut image2, n);
+    print_image(&image2);
+    println!("transposing:");
+    let mut image3 = image.to_owned();
+    transpose(&mut image3, n);
+    print_image(&image3);
+    println!("rotating ccw:");
+    let mut image4 = image.to_owned();
+    rotate_ccw(&mut image4, n, 1);
+    print_image(&image4);
+    // print_image(&rotate_ccw(&rule.0));
+    // println!();
+    // print_image(&rotate_ccw(&rotate_ccw(&rule.0)));
+    // println!();
+    // print_image(&rotate_ccw(&rotate_ccw(&rotate_ccw(&rule.0))));
 
     if image == rule.0 {
         1
