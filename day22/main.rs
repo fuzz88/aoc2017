7f1c0936ca17ed035eb31e8c8242823196c94a19
fuzz88 <ivan@oschepkov.ru>
2025-07-26 22:26:53 +0800

    [day22] cleanup

--- day22/main.rs
+++ day22/main.rs
@@ -26,11 +26,11 @@ fn read_input(filename: &str) -> Result<Nodes, Box<dyn error::Error>> {
     let n = usize::isqrt(map.len());
     let d = n / 2;
 
-    let mut nodes = HashSet::new();
+    let mut nodes = Nodes::new();
 
     for i in 0..n {
         for j in 0..n {
-            if map[i * (n + 1) + j] == 35 {
+            if map[i * (n + 1) + j] == b'#' {
                 let x = j as i64 - d as i64;
                 let y = i as i64 - d as i64;
                 nodes.insert((x, y));
@@ -50,55 +50,7 @@ fn next_coordinates(coordinates: (i64, i64), direction: &Direction) -> (i64, i64
     }
 }
 
-fn next_direction_simple(direction: &Direction, infected: bool) -> Direction {
-    match (direction, infected) {
-        (Direction::Up, true) => Direction::Right,
-        (Direction::Up, false) => Direction::Left,
-        (Direction::Right, true) => Direction::Down,
-        (Direction::Right, false) => Direction::Up,
-        (Direction::Down, true) => Direction::Left,
-        (Direction::Down, false) => Direction::Right,
-        (Direction::Left, true) => Direction::Up,
-        (Direction::Left, false) => Direction::Down,
-    }
-}
-
-fn part1(nodes: &Nodes) -> usize {
-    // Given your actual map, after 10000 bursts of activity,
-    // how many bursts cause a node to become infected?
-    // (Do not count nodes that begin infected.)
-
-    let mut infected_nodes = nodes.clone();
-
-    let mut current_direction = Direction::Up;
-    let mut current_coordinates = (0, 0);
-
-    let mut burst_count = 0;
-    let mut infections = 0;
-
-    loop {
-        if infected_nodes.contains(&current_coordinates) {
-            current_direction = next_direction_simple(&current_direction, true);
-            infected_nodes.remove(&current_coordinates);
-        } else {
-            current_direction = next_direction_simple(&current_direction, false);
-            infected_nodes.insert(current_coordinates);
-            infections += 1;
-        }
-
-        current_coordinates = next_coordinates(current_coordinates, &current_direction);
-
-        burst_count += 1;
-
-        if burst_count == 10000 {
-            break;
-        }
-    }
-
-    infections
-}
-
-fn next_direction_complex(direction: &Direction, status: &Status) -> Direction {
+fn next_direction(direction: &Direction, status: &Status) -> Direction {
     match (direction, status) {
         (Direction::Up, Status::Clean) => Direction::Left,
         (Direction::Up, Status::Weakened) => Direction::Up,
@@ -122,14 +74,12 @@ fn next_direction_complex(direction: &Direction, status: &Status) -> Direction {
     }
 }
 
-fn part2(nodes: &Nodes) -> usize {
-    // Given your actual map, after 10000000 bursts of activity,
+fn part1(nodes: &Nodes) -> usize {
+    // Given your actual map, after 10000 bursts of activity,
     // how many bursts cause a node to become infected?
     // (Do not count nodes that begin infected.)
 
     let mut infected_nodes = nodes.clone();
-    let mut weakened_nodes = HashSet::new();
-    let mut flagged_nodes = HashSet::new();
 
     let mut current_direction = Direction::Up;
     let mut current_coordinates = (0, 0);
@@ -139,19 +89,56 @@ fn part2(nodes: &Nodes) -> usize {
 
     loop {
         if infected_nodes.contains(&current_coordinates) {
-            current_direction = next_direction_complex(&current_direction, &Status::Infected);
+            current_direction = next_direction(&current_direction, &Status::Infected);
+            infected_nodes.remove(&current_coordinates);
+        } else {
+            current_direction = next_direction(&current_direction, &Status::Clean);
+            infected_nodes.insert(current_coordinates);
+            infections += 1;
+        }
+
+        current_coordinates = next_coordinates(current_coordinates, &current_direction);
+
+        burst_count += 1;
+
+        if burst_count == 10000 {
+            break;
+        }
+    }
+
+    infections
+}
+
+fn part2(nodes: &Nodes) -> usize {
+    // Given your actual map, after 10000000 bursts of activity,
+    // how many bursts cause a node to become infected?
+    // (Do not count nodes that begin infected.)
+
+    let mut infected_nodes = nodes.clone();
+    let mut weakened_nodes = Nodes::new();
+    let mut flagged_nodes = Nodes::new();
+
+    let mut current_direction = Direction::Up;
+    let mut current_coordinates = (0, 0);
+
+    let mut burst_count = 0;
+    let mut infections = 0;
+
+    loop {
+        if infected_nodes.contains(&current_coordinates) {
+            current_direction = next_direction(&current_direction, &Status::Infected);
             infected_nodes.remove(&current_coordinates);
             flagged_nodes.insert(current_coordinates);
         } else if weakened_nodes.contains(&current_coordinates) {
-            current_direction = next_direction_complex(&current_direction, &Status::Weakened);
+            current_direction = next_direction(&current_direction, &Status::Weakened);
             weakened_nodes.remove(&current_coordinates);
             infected_nodes.insert(current_coordinates);
             infections += 1;
         } else if flagged_nodes.contains(&current_coordinates) {
-            current_direction = next_direction_complex(&current_direction, &Status::Flagged);
+            current_direction = next_direction(&current_direction, &Status::Flagged);
             flagged_nodes.remove(&current_coordinates);
         } else {
-            current_direction = next_direction_complex(&current_direction, &Status::Clean);
+            current_direction = next_direction(&current_direction, &Status::Clean);
             weakened_nodes.insert(current_coordinates);
         }
 
