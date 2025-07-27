33bcaecf9abb45e79c0ce94befdf6b9f334d4728
fuzz88 <ivan@oschepkov.ru>
2025-07-27 14:27:36 +0800

    [day24] reading input

--- day24/main.rs
+++ day24/main.rs
@@ -1,5 +1,23 @@
 use std::env;
 use std::error;
+use std::fs;
+
+type Component = (u32, u32);
+
+fn read_input(filename: &str) -> Result<Vec<Component>, Box<dyn error::Error>> {
+    let components = fs::read_to_string(filename)?
+        .lines()
+        .map(|line| {
+            let mut ports = line.split("/");
+            (
+                ports.next().unwrap().parse().unwrap(),
+                ports.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect();
+
+    Ok(components)
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("--- Day24: Electromagnetic Moat ---");
@@ -8,7 +26,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         .nth(1)
         .ok_or("no input file as cli argument is provided")?;
 
-    println!("{}", input_file);
+    let input_data = read_input(&input_file)?;
+
+    println!("{:?}", input_data);
+
 
     Ok(())
 }
